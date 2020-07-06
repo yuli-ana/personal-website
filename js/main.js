aed2ba7c147877af30a98fbc63b43694091521fc
Yuliana Hazda <yulianahazda@gmail.com>
2020-07-06 00:56:24 -0400

    Add  an event listener on .btn-menu element

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const portfolio = {};
+const buttonMenu = $('.btn__menu');
+const navList = $('.nav__list');
+
+
+portfolio.toggleMenu = function() {
+    buttonMenu.on('click', () => {
+        navList.toggle();
+    })
+}
+
+
+portfolio.init = function(){
+    this.toggleMenu();
+}
 
 
 
-
-$(function(){});
+$(function(){
+    portfolio.init();
+});
