d99b8face457d0938bede786b18ab3124957c77e
Yuliana Hazda <yulianahazda@gmail.com>
2020-07-07 21:31:27 -0400

    Utilize jquery methods to add/remove classes on specific event

--- js/main.js
+++ js/main.js
@@ -4,35 +4,53 @@ const navList = $('.nav__list');
 const btnContact = $('.btn__contact');
 
 
-portfolio.toggleMenu = function() {
-    buttonMenu.on('click', () => {
-        navList.toggle();
+portfolio.toggleMenu = function () {
+    buttonMenu.on('click', (e) => {
+        e.preventDefault();
+        navList.toggleClass('opacity');
     })
 }
 
-portfolio.toggleContact = () => {
-    btnContact.on('click', function(){
-        $('.offcanvas__wrap').toggleClass('wrap__right');
-        $('.footer').toggleClass('offcanvas__visible');
+portfolio.openContact = () => {
+    btnContact.on('click', function () {
+        $('.offcanvas__wrap').addClass('wrap__right');
+        $('.footer').addClass('offcanvas__visible');
+        buttonMenu.addClass('hide');
+        $('.header').addClass('flex--start');
+        $('.scroll__indicator').addClass('left');
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    })
+}
+
+portfolio.closeContact = () => {
+    $('.btn__circle').on('click', function () {
+        $('.offcanvas__wrap').removeClass('wrap__right');
+        $('.footer').removeClass('offcanvas__visible');
+        buttonMenu.removeClass('hide');
+        $('.header').removeClass('flex--start');
+        $('.scroll__indicator').removeClass('left');
     })
 }
 
 portfolio.scroll = () => {
-    $('.btn__about').on('click', function() {
+    $('.btn__about').on('click', function () {
         $('html,body').animate({
-            scrollTop: $('.about').offset().top},
-            'slow'); 
+            scrollTop: $('.about').offset().top
+        },
+            'slow');
     });
 }
 
-portfolio.init = function(){
+
+portfolio.init = function () {
+    this.closeContact();
+    this.openContact();
     this.toggleMenu();
-    this.toggleContact();
     this.scroll();
     this.chart();
 }
 
 
-$(function(){
+$(function () {
     portfolio.init();
 });
