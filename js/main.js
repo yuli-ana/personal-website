ca0b79582b505d652ac3b458a327539fceecff66
Yuliana Hazda <yulianahazda@gmail.com>
2020-07-16 22:48:09 -0400

    Add particles library, update styles for header, create  a new component

--- js/main.js
+++ js/main.js
@@ -34,7 +34,6 @@ portfolio.addClickEventListeners = function () {
     });
 
     this.btnContact.on('click', () => {
-
         if ($(window).scrollTop() < 50) {
             this.openContactFormHandler();
         } else {
@@ -53,7 +52,7 @@ portfolio.openContactFormHandler = function () {
     this.offCanvas.addClass('offcanvas__wrap--right');
     this.buttonMenu.addClass('hide');
     this.header.addClass('flex--start');
-    this.scrollIndicator.addClass('scroll-indicator--left');
+    this.scrollIndicator.addClass('scroll-type1--left');
 }
 
 /**
@@ -64,7 +63,7 @@ portfolio.closeContactFormHandler = function () {
     this.footer.removeClass('offcanvas__visible');
     this.buttonMenu.removeClass('hide');
     this.header.removeClass('flex--start');
-    this.scrollIndicator.removeClass('scroll-indicator--left');
+    this.scrollIndicator.removeClass('scroll-type1--left');
 
     // Hide successful message
     $('.form__success').hide();
@@ -119,11 +118,126 @@ portfolio.submitForm = function () {
     })
 }
 
+portfolio.particles = function() {
+    particlesJS("particles-js", {
+        particles: {
+          number: {
+            value: 80,
+            density: {
+              enable: true,
+              value_area: 2000
+            }
+          },
+          color: {
+            value: ["#2b3c51", "#00A496", "#E03D4A", "#FF2000", "#FFCF00"]
+          },
+          shape: {
+            type: ["circle"],
+            stroke: {
+              width: 0,
+              color: "#fff"
+            },
+            polygon: {
+              nb_sides: 5
+            },
+            image: {
+              src: "https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png",
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 1,
+            random: false,
+            anim: {
+              enable: false,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false
+            }
+          },
+          size: {
+            value: 5,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 10,
+              size_min: 10,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#808080",
+            opacity: 0.4,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 5,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          }
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: {
+              enable: true,
+              mode: "grab"
+            },
+            onclick: {
+              enable: true,
+              mode: "push"
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 140,
+              line_linked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 400,
+              size: 40,
+              duration: 2,
+              opacity: 8,
+              speed: 3
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        retina_detect: true
+      });      
+}
+
+
 portfolio.init = function () {
     this.addClickEventListeners();
     this.handleButtonAbout();
     this.closeOnScroll();
     this.submitForm();
+    this.particles();
 }
 
 
